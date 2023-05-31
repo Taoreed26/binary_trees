525410cb25d47a14aa4795d51bd1935ced62eee1
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-05-31 18:11:28 +0100

    Create 100-binary_trees_ancestor.c

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,54 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_depth - gives actual  depth of node in a binary_tree.
+ *@tree: points to the node that gives actual depth.
+ * Return: returns zero
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	else
+		return (1 + binary_tree_depth(tree->parent));
+}
+/**
+ * binary_trees_ancestor - finds the lowest common ancestor of two nodes
+ * @first: first node
+ * @second: second node
+ * Return: lowest common ancestor
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				     const binary_tree_t *second)
+{
+	size_t height_f = 0, height_s = 0;
+	const binary_tree_t *aux = NULL;
+
+	height_f = binary_tree_depth(first);
+	height_s = binary_tree_depth(second);
+	if (height_f && height_s)
+	{
+		if (height_f > height_s)
+		{
+			aux = first;
+			first = second;
+			second = aux;
+		}
+		while (first)
+		{
+			aux = second;
+			while (aux)
+			{
+				if (first == aux)
+					return ((binary_tree_t *)first);
+				aux = aux->parent;
+			}
+			first = first->parent;
+		}
+	}
+	else
+	{
+		return (NULL);
+	}
+	return (NULL);
+}
