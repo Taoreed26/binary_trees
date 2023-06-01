1e28fccb70347b9392121bbbcec68c3441d3ae3d
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 16:01:44 +0100

    Create 120-binary_tree_is_avl.c

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_avl: evaluates if it is AVL
+ * @tree: what is expected to be evaluated
+ * Return: returns
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	return (1);
+}
