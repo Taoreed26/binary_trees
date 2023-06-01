2cd57652244a2f0ebd3725854ebf38eec5e8fea1
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 20:02:23 +0100

    Create 122-array_to_avl.c

---
+++ 122-array_to_avl.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * array_to_avl:this is a function used for creating
+ * @array: array with nodes
+ * @size: sizes the array
+ * Return: points
+ */
+avl_t *array_to_avl(int *array, size_t size)
+{
+	return (NULL);
+}
