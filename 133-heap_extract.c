22de1fa43e33b22edbe8ca976fc4dcbc433dc9d4
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 21:20:29 +0100

    Create 133-heap_extract.c

---
+++ 133-heap_extract.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * heap_extract - used as an extractor
+ * @root: evaluates the root
+ * Return: returns value
+ */
+int heap_extract(heap_t **root)
+{
+	return (0);
+}
