7c853eb8d25bd3635ac87a89ca67695fa37f62c5
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 21:17:53 +0100

    Create 132-array_to_heap.c

---
+++ 132-array_to_heap.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * array_to_heap: creates a heap
+ * @array: used in referencing array
+ * @size: denotes the size
+ * Return: points
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	return (NULL);
+}
