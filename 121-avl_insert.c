f9f59c69a94ce033063ea0ba5af50863492733d1
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 16:03:39 +0100

    Create 121-avl_insert.c

---
+++ 121-avl_insert.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * avl_insert: inserts node
+ * @tree: this is the root to be evaluated
+ * @value: inserts Node
+ * Return: this serves as a pointer
+ */
+avl_t *avl_insert(avl_t **tree, int value);
+{
+	return (NULL);
+}
