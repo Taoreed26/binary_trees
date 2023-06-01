c8521a74e46f06acbb6c654cdd489adf0b186309
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 21:14:14 +0100

    Create 130-binary_tree_is_heap.c

---
+++ 130-binary_tree_is_heap.c
@@ -0,0 +1,10 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_heap: checker for the heap tree
+ * @tree:evaluates te root
+ * Return: returns high or low
+ */
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	return (1);
+}
