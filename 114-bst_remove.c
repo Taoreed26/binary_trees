da205b74dc30b0085f7bf85fbf97763ba1c7fab4
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 12:01:12 +0100

    Create 114-bst_remove.c

---
+++ 114-bst_remove.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * bst_remove: removes the necessary 
+ * @root: evaluates the root
+ * @value: finds the node
+ * Return: returns the pointer
+ */
+bst_t *bst_remove(bst_t *root, int value)
+{
+	return (NULL);
+}
