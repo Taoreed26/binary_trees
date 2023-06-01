c1b1d51ed2440a4b90c276d1daea8f3b3f535f05
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 21:16:00 +0100

    Create 131-heap_insert.c

---
+++ 131-heap_insert.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * heap_insert: functions as an insertion key
+ * @root:evaluates the root
+ * @value: relates the insertion
+ * Return: points
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	return (NULL);
+}
