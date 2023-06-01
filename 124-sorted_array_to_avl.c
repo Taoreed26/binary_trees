387a6b5be2003e0e97b6080c369bf8e651e0fdaf
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 20:06:03 +0100

    Create 124-sorted_array_to_avl.c

---
+++ 124-sorted_array_to_avl.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * sorted_array_to_avl: a function used for creating
+ * @array: used for evaluation
+ * @size: search engine
+ * Return: points
+ */
+avl_t *sorted_array_to_avl(int *array, size_t size)
+{
+	return (NULL);
+}
