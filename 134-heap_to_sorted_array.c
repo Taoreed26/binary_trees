da36976d7384e341a25c145d241104c547701639
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 21:25:11 +0100

    Create 134-heap_to_sorted_array.c

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * heap_to_sorted_array: used to convert binary map to sorted array
+ * @heap: works on the root
+ * @size: cannotes how big or small the array is
+ * Return: points
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	return (NULL);
+}
