17836c4a2e2eaa592f2abac3c1cc90b613c2bd7d
Taoreed26 <117745831+Taoreed26@users.noreply.github.com>
2023-06-01 20:04:34 +0100

    Create 123-avl_remove.c

---
+++ 123-avl_remove.c
@@ -0,0 +1,11 @@
+#include "binary_trees.h"
+/**
+ * avl_remove: removal function
+ * @root: the tree root
+ * @value: used as a search engine
+ * Return: points
+ */
+avl_t *avl_remove(avl_t *root, int value)
+{
+	return (NULL);
+}
